09434b1cef0737706c18427a8ad3be4880485345
Wahaj khan <154734596+wahaj906@users.noreply.github.com>
2024-02-23 18:16:42 +0500

    Add files via upload
    javascript-practice

---
+++ promot/app.js
@@ -0,0 +1,23 @@
+// prompht se hum user se input lete hain
+let num1 = prompt("First Value");
+let num2 = prompt("Second Value");
+
+// string ko number me convert kiya hai
+num1 = Number(num1);
+num2 = Number(num2);
+
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+console.log("Addition : " + sum);
+console.log("Minus : " + minus);
+console.log("Multiplication : " + multiply);
+console.log("Division : " + divide);
+// let num1 = prompt("First Value",'wahaj');
+
+
+let wahaj;  //undefined
+shahwaiz = 50;
+console.log(wahaj);
